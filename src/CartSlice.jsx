1c470bd51fb7ed3a80b05cee75317c0b259505b0
TaiCorraggio <taicorraggio@gmail.com>
2025-06-11 21:46:03 -0400

    Partial functionality

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -22,8 +22,8 @@ export const CartSlice = createSlice({
     const { name, quantity } = action.payload;
         const itemToUpdate = state.items.find(item => item.name === name);
         if (itemToUpdate) {
-        itemToUpdate.quantity = quantity;
-     }
+            itemToUpdate.quantity = quantity;
+        }
     },
   },
 });
